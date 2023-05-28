2af09dcc91dfd95c768f9358118438fab03929ed
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-28 22:00:07 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -10,7 +10,7 @@ from main.inline import button1
 
 def trim_title(title: str):
     title = title.replace("Demon Slayer S04E04 1080p WEB H.264 E-AC-3 -Yandere-Raws (AMZN) (Kimetsu no Yaiba: Katanakaji no Sato-hen)", "Demon Slayer Kimetsu No Yaiba To the Swordsmith Village - 04")
-    title = title.replace("Demon Slayer S04E05 1080p WEB H.264 E-AC-3 -Yandere-Raws (AMZN) (Kimetsu no Yaiba: Katanakaji no Sato-hen)", "Demon Slayer Kimetsu No Yaiba To the Swordsmith Village - 05")
+    title = title.replace("Demon Slayer S04E08 1080p WEB H.264 E-AC-3 -Yandere-Raws (AMZN) (Kimetsu no Yaiba: Katanakaji no Sato-hen)", "Demon Slayer Kimetsu No Yaiba To the Swordsmith Village - 08")
     ext = ".mkv"
     title = title + ext
     return title
