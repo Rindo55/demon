d7dd5cb8a800bd8d2f4ed6b85bd849d7850300c8
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-05-28 22:14:59 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -126,7 +126,7 @@ async def start_uploading(data):
         duration = get_duration(file)
         durationx = get_durationx(file)
         filed = os.path.basename(file)
-        filed = filed.replace("Demon Slayer S04E08 1080p WEB H.264 E-AC-3 -Yandere-Raws (AMZN)", "Demon Slayer S4 - 08 [1080p Web-DL]")
+        filed = filed.replace("Demon Slayer S04E08 1080p WEB x264 E-AC-3 -Tsundere-Raws", "Demon Slayer S4 - 08 [1080p Web-DL]")
         razo = filed.replace("[1080p Web-DL].mkv", "[720p x265] @animxt.mkv")
         razo = filed.replace("[1080p Web-DL].mkv", "[720p x265] @animxt.mkv")
         fpath = "downloads/" + filed
