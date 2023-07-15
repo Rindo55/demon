dd4bf46e3ed6ed0c9e824bd79158dc2b71ff8d70
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-07-15 23:19:33 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -126,7 +126,7 @@ async def start_uploading(data):
         duration = get_duration(file)
         durationx = get_durationx(file)
         filed = os.path.basename(file)
-        filed = filed.replace("Bleach S18E01 1080p WEB H.264 AAC -Tsundere-Raws (HULU)", "Bleach TYBW Separation - 01 [1080p Web-DL]")
+        filed = filed.replace("BLEACH.Thousand-Year.Blood.War.S02E02.PEACE.FROM.SHADOWS.1080p.DSNP.WEB-DL.AAC2.0.x264", "Bleach TYBW Separation - 02 [1080p Web-DL]")
         razo = filed.replace("[1080p Web-DL].mkv", "[720p x265] @animxt.mkv")
         razo = filed.replace("[1080p Web-DL].mkv", "[720p x265] @animxt.mkv")
         fpath = "downloads/" + filed
