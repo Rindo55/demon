52c949c78c452eb96262b2fca54b665af6a13e7b
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-10-05 00:00:02 +0530

    Update parser.py

--- main/modules/parser.py
+++ main/modules/parser.py
@@ -10,13 +10,13 @@ from main.inline import button1
 
 def trim_title(title: str):
     title = title.replace("Demon Slayer S04E04 1080p WEB H.264 E-AC-3 -Yandere-Raws (AMZN) (Kimetsu no Yaiba: Katanakaji no Sato-hen)", "Demon Slayer Kimetsu No Yaiba To the Swordsmith Village - 04")
-    title = title.replace("Bleach S17E24 1080p WEB H.264 AAC -Tsundere-Raws (DSNP) (Multi-Subs, VOSTFR, Bleach: Sennen Kessen-hen - Ketsubetsu-tan, Bleach: Thousand-Year Blood War - The Separation)", "Bleach Thousand Year Blood War The Separation - 11")
+    title = title.replace("[CameEsp] Tokyo Revengers - Tenjiku Hen - 01 [1080p][ESP-ENG][mkv]", "Tokyo Revengers - Tenjiku Hen - 01")
     ext = ".mkv"
     title = title + ext
     return title
 
 def parse():
-    a = feedparser.parse("https://siftrss.com/f/lwk3yAnbNv")
+    a = feedparser.parse("https://nyaa.si/?page=rss&u=Mayansito&q=tokyo%201080p")
     b = a["entries"]
     b = b[0:1]
     data = []    
